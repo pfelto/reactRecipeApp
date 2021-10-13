026a7f0fe25f9b5f2952c8f9f511289ed6ab554a
pfelto <paul.r.felton@gmail.com>
2021-10-13 14:47:41 -0400

    code for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,23 @@
+const path = require('path');
+
 module.exports = {
-    entry: './'
-}
+    entry: './index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env', 'stage-0', 'react']
+              }
+            }
+          }
+        ]
+      }
+  };
